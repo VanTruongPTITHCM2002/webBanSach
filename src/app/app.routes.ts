6a1099b728eab03ab17784a7d59ba3bb1d6c0cf9
VanTruongPTITHCM2002 <104205896+VanTruongPTITHCM2002@users.noreply.github.com>
2025-01-16 14:12:34 +0700

    tiep tuc

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,12 +5,14 @@ import { NotFoundComponent } from './not-found/not-found.component';
 import { CategoryComponent } from './category/category.component';
 import { AuthComponent } from './auth/auth.component';
 import { DetailComponent } from './detail/detail.component';
+import { BookComponent } from './book/book.component';
 
 export const routes: Routes = [
     {path:'',component:HomeComponent,title:"Trang chủ"},
     {path:'auth/login',component:AuthComponent,title:"Đăng nhập"},
     {path:'auth/signup',component:AuthComponent,title:"Đăng ký"},
-    {path:'detail',component:DetailComponent,title:"Chi tiết sách"}
+    {path:'detail',component:DetailComponent,title:"Danh sách sách"},
+    {path:'detail/book',component:BookComponent,title:"Chi tiết sách"},
     // { path: 'contact', redirectTo: '/lien-he', pathMatch: 'full' },
     // { path: 'category/:id', component:CategoryComponent },
     // { path: '**', component: NotFoundComponent }
